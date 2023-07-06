9cc2014c6e42210d18d6fd560507e19483dbda21
Gurpal Sagoo <gurpz1@users.noreply.github.com>
2023-07-06 14:25:05 +0100

    Add image optimization to vite

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ViteImageOptimizer()
   ],
   resolve: {
     alias: {
